c376c2f758366dee82d35184df417813798e9074
John Damask <johndamask@Johns-MacBook-Pro-2.local>
2025-11-27 08:42:44 -0500

    Update launch banner logo to 8-bit block art style

--- pkg/ui/logo.go
+++ pkg/ui/logo.go
@@ -1,13 +1,12 @@
 package ui
 
-// 8-bit dog in green
+// 8-bit dog in green using block characters
 // ANSI color: \033[38;5;46m (Green)
 const Logo = "\033[38;5;46m" + `
-      __
- (___() \
- /      |
-/  __   |
-\  \ \  \
- \  \ \  \
-  \__\ \__\
+    ██      ██
+    ██████████
+    ██  ██  ██
+    ██████████
+    ██      ██
+    ██      ██
 ` + "\033[0m"
